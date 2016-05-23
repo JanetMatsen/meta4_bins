e5f763b3b8622346d31ff38ee0c2afb84b941787
Janet Matsen <janetmatsen@gmail.com>
2016-05-23 15:19:25 -0700

    added program & SQL paths, & job e-mails

--- globals.sh
+++ globals.sh
@@ -1,2 +1,12 @@
 HOST=`mysql_host`
 DB=meta4_bins
+
+# pathes
+BWA=/work/software/bwa/bin/bwa
+SAMTOOLS=/work/software/samtools/bin/samtools
+MYSQL=mysql
+MYSQL_HOST=/work/software/bin/mysql_host
+HTSEQ_COUNT=/work/software/htseq/bin/htseq-count
+
+# job parameters
+EMAIL=dacb@uw.edu,jmatsen@uw.edu
