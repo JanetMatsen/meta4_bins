66a91931c6b5ea927ac8d5ca4b5a05c15b967d33
Janet Matsen <janetmatsen@gmail.com>
2016-05-23 17:20:43 -0700

    fixed output so all .gff are correct (no .fngff)
    Also checked that each of the .gff files actually
    exist.
    ** Need to do that for the isolates **
    Currently there are not .gff file so the checking
    step doesnt' happen for them.

--- data/bins/summarise_bins.py
+++ data/bins/summarise_bins.py
@@ -6,19 +6,32 @@ import re
 
 import pandas as pd
 
-fasta_paths = list(glob.iglob('./*/bins/*.fasta', recursive=True))
+fasta_paths = glob.glob('./*/bins/*.fasta')
 # append on Dave's, which end with .fna
-fasta_paths += list(glob.iglob('./*/bins/*.fna', recursive=True))
+fasta_paths += glob.glob('./*/bins/*.fna')
 
 print("number of fasta paths found: {}".format(len(fasta_paths)))
 
 def build_summary_table():
     bin_info_dicts = []
     for filepath in fasta_paths:
+        # get the full path from glob:
         full_path = os.path.abspath(filepath)
+        # e.g. /work/meta4_bins/data/samples/LakWasMeta1_LOW4_2/Raw_Data/8904.1.115212.CGATGT.fastq.gz
         info_dict = {'fasta path' : full_path}
+
         # assume there is an equally named file but .gff
-        info_dict['gff path'] = info_dict['fasta path'].rstrip('fasta') + 'gff'
+        if ".fasta" in info_dict['fasta path']:
+            info_dict['gff path'] = info_dict['fasta path'].rstrip('fasta') + 'gff'
+        elif ".fna" in info_dict['fasta path']:
+            info_dict['gff path'] = info_dict['fasta path'].rstrip('fna') + 'gff'
+        # assert that this file exists:
+        # TODO: take off this "if" that prevents checking of isolates. 
+        # Requires copying the isolates over for good. 
+        if "isolate" not in full_path:
+            assert os.path.isfile(info_dict['gff path']), \
+                "file {} doesn't exist!".format(info_dict['gff path'])
+        
 
         # get the name of the bin
         file_name = os.path.basename(filepath)
@@ -37,6 +50,8 @@ if __name__ == "__main__":
     summary_table_2 = summary_table[['name', 'fasta path', 'gff path']]
     summary_table_2.to_csv('bin_summary_table.tsv',
                          sep='\t', index=False)
+    
+    summary_table_2.head(3)
 
     # copy a version of this to /work/meta4_bins/job_manager as bins.tsv
     summary_table_2.to_csv('/work/meta4_bins/job_manager/bins.tsv', sep='\t', index=False)
