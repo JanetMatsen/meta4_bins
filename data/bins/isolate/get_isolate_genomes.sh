2ff9b811e83d1ba846457af159e97b3031840330
Janet Matsen <janetmatsen@gmail.com>
2016-05-24 16:07:48 -0700

    final version: scripts in separate dir + README added

--- data/bins/isolate/get_isolate_genomes.sh
+++ data/bins/isolate/get_isolate_genomes.sh
@@ -9,10 +9,10 @@ source activate m4_janalysis
 
 # copy the files from ./originals/ with better names
 echo "copy the bins over"
-python prepare_bins.py
+python ./scripts/prepare_bins.py
 
 echo "make .gff for each fasta"
 # Make a .gff file for each fasta:
-./make_gff_from_fna.sh
+./scripts/make_gff_from_fna.sh
 
 
