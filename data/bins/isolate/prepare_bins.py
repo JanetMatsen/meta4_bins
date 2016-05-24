52b561e61e9f1af6c061e0cddabb5ada5f89fdb7
Janet Matsen <janetmatsen@gmail.com>
2016-05-24 15:34:43 -0700

    also copies fasta files w/ good names.

--- data/bins/isolate/prepare_bins.py
+++ data/bins/isolate/prepare_bins.py
@@ -10,18 +10,29 @@ def get_original_paths():
     return genbank_path_list, fasta_path_list 
 
 
-def file_name_from_description(name):
+def file_name_from_genbank_description(name):
     """
     Acidovorax-127 (UID4105), whole genome shotgun sequence." --> Acidovorax-127 
     """
     # Note that the - in the brackets has to come at the end. 
-    m = re.search("([\w\s-]+) \([\w]+\), [\w.]+", \
-        name)
+    m = re.search("([\w\s-]+) \([\w]+\), [\w.]+", name)
     if m:
         return m.group(1)
     else:
         return "???"
-    pass
+
+
+def file_name_from_fasta_description(name):
+    """
+    Ga0081612_1001 [organism=Methylophilus methylotrophus-127-2 (UID203)] [tech=wgs] [topology=linear] [gcode=11] [completedness=partial] [project=0]
+    --> Methylophilus_methylotrophus-127-2
+    """
+    # Note that the - in the brackets has to come at the end. 
+    m = re.search("[\w_]+ \[organism=([\w\s-]+) \([\w]+\)\]", name)
+    if m:
+        return m.group(1)
+    else:
+        return "???"
     
 
 def new_genbank_name(genbank_path):
@@ -29,11 +40,24 @@ def new_genbank_name(genbank_path):
     # seq_records is a generator.  Just use the first one.
     for seq_record in seq_records:
         print(seq_record.description)
-        new_name = file_name_from_description(seq_record.description)
+        new_name = file_name_from_genbank_description(seq_record.description)
         # we don't actually want to loop over the rest, so break out
         break
     # replace spaces with _:
     new_name = new_name.replace(" ", "_")
+    return new_name
+
+
+def new_fasta_name(fasta_path):
+    seq_records = SeqIO.parse(fasta_path, "fasta")
+    for seq_record in seq_records:
+        print(seq_record.description)
+        new_name = file_name_from_fasta_description(seq_record.description)
+        # we don't actually want to loop over the rest, so break out
+        break
+    # replace spaces with _:
+    new_name = new_name.replace(" ", "_")
+    print("new name for {}: {}".format(fasta_path, new_name))
     return new_name
 
 
@@ -44,14 +68,25 @@ def copy_genbank_with_new_name(genbank_path):
     subprocess.check_call(['mv', genbank_path, new_path])
 
 
+def copy_fasta_with_new_name(fasta_path):
+    new_path = "./bins/" + new_fasta_name(fasta_path) + '.fsa'
+    print("copying {} to {}".format(fasta_path, new_path))
+    subprocess.check_call(['mv', fasta_path, new_path])
+    
+    pass
+
+
 def copy_all_files():
     genbank_path_list, fasta_path_list = get_original_paths()    
     for gbf in genbank_path_list:
         copy_genbank_with_new_name(gbf)
+    for fna in fasta_path_list:
+        copy_fasta_with_new_name(fna)
 
 
 if __name__ == "__main__":
     print('running script')
     copy_all_files()
+
 
 
