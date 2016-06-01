7400190015105004d86780efe961d74570ec3678
Janet Matsen <janetmatsen@gmail.com>
2016-06-01 04:57:49 -0700

    isolates are now .fasta, not .fna
    to be consisted with the Fauzi/Dave bins.

--- data/bins/isolate/scripts/make_gff_from_fna.sh
+++ data/bins/isolate/scripts/make_gff_from_fna.sh
@@ -7,7 +7,8 @@ do
     gbf=`echo $file | sed "s/fsa/gbf/g"`
     organism=`head -1 $file | awk -F'=' '{ split($2, array, "("); printf("%s", array[1]); }'`
     organism=`echo $organism | sed "s/ /_/g"`
-    mv $file "./bins/$organism.fasta"
+    # We want .fna (to match the bins)
+    # mv $file "./bins/$organism.fasta"
     mv $gbf "./bins/$organism.genbank"
     java -jar /work/software/readseq/readseq.jar -inform=genbank -format=gff "./bins/$organism.genbank"
     mv "./bins/$organism.genbank.gff" "./bins/$organism.gff"
