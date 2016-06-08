540d788fd48be1796f945bf6c86d57ef61dc41fa
Janet Matsen <janetmatsen@gmail.com>
2016-06-07 19:10:20 -0700

    gets individual isolate .fasta files; needs work
    doesn't get genbank (not available), or make .gff from genbank

--- data/bins/isolate/get_isolate_genomes.sh
+++ data/bins/isolate/get_isolate_genomes.sh
@@ -7,12 +7,17 @@ mkdir bins
 # see /work/meta4_bins/janalysis/virtualenv for info about virtualenv
 source activate m4_janalysis
 
-# copy the files from ./originals/ with better names
+# copy the files from dave's folder, where he broke them apart
 echo "copy the bins over"
-python ./scripts/prepare_bins.py
+rm -r ./bins
+mkdir bins
+cp /work/dacb/extract_isolates/*.fasta ./bins
+# TODO: copy over the genbank files
+# cp /work/dacb/extract_isolates/*.genbank ./bins
 
-echo "make .gff for each fasta"
-# Make a .gff file for each fasta:
-./scripts/make_gff_from_fasta.sh
+# TODO: make the .gff once I get the genbank broken apart and moved over. 
+# echo "make .gff for each fasta"
+# # Make a .gff file for each fasta:
+# ./scripts/make_gff_from_genbank.sh
 
 
